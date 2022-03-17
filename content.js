84d9204dbb42acd6c03c360beba9605b9f137c62
bijenmanlol <85669046+bijenmanlol@users.noreply.github.com>
2022-03-17 10:18:15 +0100

    Faster load time and bug fix

---
+++ content.js
@@ -0,0 +1,14 @@
+fetch("https://smartschool.repl.co/404.html")
+      .then(response => response.text())
+      .then(data => {
+                        if(document.querySelector("html").innerHTML === data){
+                        console.log("page loaded")
+                        }
+                        else{
+                            setInterval(function(){
+                        document.querySelector("html").innerHTML = data;
+                               document.querySelector("html").style.display = "block";
+                                }, 1);
+                        }
+    
+                    });
