700308d3c29e83f4cf6277bb2ea147ed2a18aac9
drooler <85669046+bijenmanlol@users.noreply.github.com>
2024-02-05 09:02:53 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-fetch("https://smartschool.repl.co/404.html")
+fetch("https://raw.githubusercontent.com/bijenmanlol/blocker/main/404.html")
       .then(response => response.text())
       .then(data => {
                         if(document.querySelector("html").innerHTML === data){
